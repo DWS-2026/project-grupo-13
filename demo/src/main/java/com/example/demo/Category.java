83f0e36164c7ad13555e58886ed848f0c02e09c3
Alvaro-223 <a.cuadrado.2024@alumnos.urjc.es>
2026-02-20 12:12:51 +0100

    ClasesJava

---
+++ demo/src/main/java/com/example/demo/Category.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class Category {
+    private String name;
+    private String description;
+
+    public Category (String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+}

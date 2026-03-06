23b8319750d0499cbda647949a60f2957ee03b90
Alvaro-223 <a.cuadrado.2024@alumnos.urjc.es>
2026-03-06 11:56:21 +0100

    Productos dinamicos con mustache

--- demo/src/main/java/com/example/demo/DataService.java
+++ demo/src/main/java/com/example/demo/DataService.java
@@ -8,7 +8,15 @@ public class DataService {
         return List.of(
             new Producto("Iphone 17", 1299, "Último modelo", "/imagenes/Iphone17.jpg", "Smartphones"),
             new Producto("Oppo Reno 12 5G", 499, "Gama media premium", "/imagenes/Oppo.jpg", "Smartphones"),
-            new Producto("Samsung Galaxy Z Flip 7", 1099, "Plegable", "/imagenes/samsung.jpg", "Smartphones")
+            new Producto("Samsung Galaxy Z Flip 7", 1099, "Plegable", "/imagenes/samsung.jpg", "Smartphones"),
+
+            new Producto("MacBook Pro", 1999, "Portátil profesional", "/imagenes/asus2.jpg", "Ordenadores"),
+            new Producto("Asus ROG Strix", 1499, "Gaming", "/imagenes/asus1.jpg", "Ordenadores"),
+
+            new Producto("RTX 4090", 1999, "GPU tope de gama", "/imagenes/grafica1.jpg", "GPU"),
+            new Producto("RTX 4070", 699, "Gama media-alta", "/imagenes/grafica3.jpg", "GPU"),
+
+            new Producto("iPad Pro", 999, "Tablet profesional", "/imagenes/iphone15.jpg", "Tablets")
         );
     }
 }
--- demo/src/main/java/com/example/demo/ProductController.java
+++ demo/src/main/java/com/example/demo/ProductController.java
@@ -5,22 +5,24 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ProductController {
 
-    @GetMapping("/Smartphones")
-    public String smartphones(Model model) {
+    @GetMapping("/categoria/{nombre}")
+    public String categoria(@PathVariable String nombre, Model model) {
 
         List<Producto> lista = DataService.getProductos()
-                .stream()
-                .filter(p -> p.getCategoria().equalsIgnoreCase("Smartphones"))
-                .toList();
+            .stream()
+            .filter(p -> p.getCategoria().equalsIgnoreCase(nombre))
+            .toList();
 
         model.addAttribute("productos", lista);
-        model.addAttribute("categoria", "Smartphones");
+        model.addAttribute("categoria", nombre);
 
-        return "Smartphones";
+        return "Categoria"; // Categoria.html
     }
+
 
 }

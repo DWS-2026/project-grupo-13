3170e964baa1de2f188b766a831b0d3314a40193
Alvaro-223 <a.cuadrado.2024@alumnos.urjc.es>
2026-03-10 09:22:55 +0100

    Productos con mustache

--- demo/src/main/java/com/example/demo/DataService.java
+++ demo/src/main/java/com/example/demo/DataService.java
@@ -6,17 +6,17 @@ public class DataService {
 
     public static List<Producto> getProductos() {
         return List.of(
-            new Producto("Iphone 17", 1299, "Último modelo", "/imagenes/Iphone17.jpg", "Smartphones"),
-            new Producto("Oppo Reno 12 5G", 499, "Gama media premium", "/imagenes/Oppo.jpg", "Smartphones"),
-            new Producto("Samsung Galaxy Z Flip 7", 1099, "Plegable", "/imagenes/samsung.jpg", "Smartphones"),
+            new Producto(1, "Iphone 17", 1299, "Último modelo", "/imagenes/Iphone17.jpg", "Smartphones"),
+            new Producto(2, "Oppo Reno 12 5G", 499, "Gama media premium", "/imagenes/Oppo.jpg", "Smartphones"),
+            new Producto(3,"Samsung Galaxy Z Flip 7", 1099, "Plegable", "/imagenes/samsung.jpg", "Smartphones"),
 
-            new Producto("MacBook Pro", 1999, "Portátil profesional", "/imagenes/asus2.jpg", "Ordenadores"),
-            new Producto("Asus ROG Strix", 1499, "Gaming", "/imagenes/asus1.jpg", "Ordenadores"),
+            new Producto(4, "MacBook Pro", 1999, "Portátil profesional", "/imagenes/asus2.jpg", "Ordenadores"),
+            new Producto(5, "Asus ROG Strix", 1499, "Gaming", "/imagenes/asus1.jpg", "Ordenadores"),
 
-            new Producto("RTX 4090", 1999, "GPU tope de gama", "/imagenes/grafica1.jpg", "GPU"),
-            new Producto("RTX 4070", 699, "Gama media-alta", "/imagenes/grafica3.jpg", "GPU"),
+            new Producto(6, "RTX 4090", 1999, "GPU tope de gama", "/imagenes/grafica1.jpg", "GPU"),
+            new Producto(7, "RTX 4070", 699, "Gama media-alta", "/imagenes/grafica3.jpg", "GPU"),
 
-            new Producto("iPad Pro", 999, "Tablet profesional", "/imagenes/iphone15.jpg", "Tablets")
+            new Producto(8, "iPad Pro", 999, "Tablet profesional", "/imagenes/iphone15.jpg", "Tablets")
         );
     }
 }

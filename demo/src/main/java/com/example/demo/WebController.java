6027b9d60ba36654b036be3409212b8647eaa0aa
ClaudioURJC <cr.horrillo.2024@alumnos.urjc.es>
2026-03-13 09:33:53 +0100

    actu

--- demo/src/main/java/com/example/demo/WebController.java
+++ demo/src/main/java/com/example/demo/WebController.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -126,23 +127,25 @@ public class WebController {
 private List<Review> todasLasValoraciones = new ArrayList<>();
 
 @GetMapping("/Product")
-public String verProducto(@RequestParam(name="id") int id, org.springframework.ui.Model model) {
-        List<Review> valoracionesFiltradas = todasLasValoraciones.stream()
-            .filter(r -> r.getProductoId() == id)
-            .toList();
+public String verProducto(@RequestParam(name="id") int id, Model model) {
 
-    // Enviamos a la vista las valoraciones específicas de este producto
+    List<Review> valoracionesFiltradas = todasLasValoraciones.stream()
+        .filter(r -> r.getProductoId() == id)
+        .toList();
+
     model.addAttribute("valoraciones", valoracionesFiltradas);
-    
+
+    model.addAttribute("productoId", id); // importante
+
     return "Product";
 }
-
 @PostMapping("/api/valoraciones")
 @ResponseBody
 public String guardarValoracion(@RequestBody Review nuevaReview) {
-    // Al recibir el JSON, Review ya debe traer el productoId desde el JavaScript
+
     todasLasValoraciones.add(nuevaReview);
-    return "Valoración guardada correctamente";
+
+    return "ok";
 }
 
 

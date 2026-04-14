6f4c57792357722907706e069d76c86b8b846b5d
Alvaro-223 <a.cuadrado.2024@alumnos.urjc.es>
2026-04-14 09:41:15 +0200

    Configuración de seguridad para API

--- demo/src/main/java/com/example/demo/Security/WebSecurityConfig.java
+++ demo/src/main/java/com/example/demo/Security/WebSecurityConfig.java
@@ -6,9 +6,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 @EnableWebSecurity
@@ -29,82 +33,110 @@ public class WebSecurityConfig {
         return authProvider;
     }
 
-
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Order(1)
+    public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
 
         http.authenticationProvider(authenticationProvider());
 
         http
-            
-            
-            .headers(headers -> headers.frameOptions(frame -> frame.disable()))
+                .securityMatcher("/api/**");
+                //.exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandlerJwt));
 
-            .authorizeHttpRequests(authorize -> authorize
-                // H2-console siempre permitido
-                .requestMatchers("/h2-console/**").permitAll()
-                
-                
-            
-                
-               
-                .requestMatchers("/css/**", "/js/**", "/imagenes/**", "/image/**", "/*.css").permitAll()
-                .requestMatchers("/", "/Index").permitAll()
-                .requestMatchers("/imagenes/**").permitAll()
-                .requestMatchers("/image/**").permitAll()
-                .requestMatchers("/CategoriesScreen").permitAll()
-                .requestMatchers("/PromotionsScreen").permitAll()
-                .requestMatchers("/producto/**").permitAll()
-                .requestMatchers("/categoria/**").permitAll()
-                .requestMatchers("/register", "/Register").permitAll()
-                .requestMatchers("/login", "/Login").permitAll()
-                .requestMatchers("/registro", "/registro/**").permitAll()
+        http
+                .authorizeHttpRequests(authorize -> authorize  //examples we have to replace
+                        // PRIVATE ENDPOINTS
+                        // Images
+                        .requestMatchers(HttpMethod.PUT, "/api/images/*/media").hasRole("USER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/books/*/images/*").hasRole("USER")
+                        // Books
+                        .requestMatchers(HttpMethod.POST, "/api/books/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.PUT, "/api/books/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/books/**").hasRole("ADMIN")
+                        // Shops
+                        .requestMatchers(HttpMethod.PUT, "/api/shops/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/shops/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/shops/**").hasRole("ADMIN")
+                        // PUBLIC ENDPOINTS
+                        .anyRequest().permitAll());
 
-                
+        // Disable Form login Authentication
+        http.formLogin(formLogin -> formLogin.disable());
 
+        // Disable CSRF protection (it is difficult to implement in REST APIs)
+        http.csrf(csrf -> csrf.disable());
 
+        // Disable Basic Authentication
+        http.httpBasic(httpBasic -> httpBasic.disable());
 
-                
-                .requestMatchers("/ShoppingCart").hasAnyRole("USER", "ADMIN")
-                .requestMatchers("/agregar-carrito/**").hasAnyRole("USER", "ADMIN")
+        // Stateless session
+        http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-                .requestMatchers("/reviews/add").hasAnyRole("USER", "ADMIN")
+        // Add JWT Token filter
+        /* 
+        http.addFilterBefore(new JwtRequestFilter(userDetailService, jwtTokenProvider),
+                UsernamePasswordAuthenticationFilter.class);
+        */
+        return http.build();
+    }
 
+    @Bean
+    @Order(2)
+    public SecurityFilterChain webfilterChain(HttpSecurity http) throws Exception {
 
-                
-                .requestMatchers("/Admin/**").hasRole("ADMIN")
-                .requestMatchers("/AdminProduct/**").hasRole("ADMIN")
-                .requestMatchers("/AdminUser/**").hasRole("ADMIN")
-                .requestMatchers("/AdminUser/promote/**").hasRole("ADMIN")
-                .requestMatchers("/AdminCategories/**").hasRole("ADMIN")
-                .requestMatchers("/ChangePassword").authenticated()
-               
-                
-                .requestMatchers("/user/profile/upload").authenticated()
+        http.authenticationProvider(authenticationProvider());
 
-                .anyRequest().authenticated()
-                
+        http
 
-                
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()))
 
+                .authorizeHttpRequests(authorize -> authorize
+                        // H2-console siempre permitido
+                        .requestMatchers("/h2-console/**").permitAll()
 
-            )
+                        .requestMatchers("/css/**", "/js/**", "/imagenes/**", "/image/**", "/*.css").permitAll()
+                        .requestMatchers("/", "/Index").permitAll()
+                        .requestMatchers("/imagenes/**").permitAll()
+                        .requestMatchers("/image/**").permitAll()
+                        .requestMatchers("/CategoriesScreen").permitAll()
+                        .requestMatchers("/PromotionsScreen").permitAll()
+                        .requestMatchers("/producto/**").permitAll()
+                        .requestMatchers("/categoria/**").permitAll()
+                        .requestMatchers("/register", "/Register").permitAll()
+                        .requestMatchers("/login", "/Login").permitAll()
+                        .requestMatchers("/registro", "/registro/**").permitAll()
 
-            .formLogin(formLogin -> formLogin
-                .loginPage("/Login")
-                .failureUrl("/Login?error")
-                .defaultSuccessUrl("/Index", true)
-                .permitAll()
-            )
+                        .requestMatchers("/ShoppingCart").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/agregar-carrito/**").hasAnyRole("USER", "ADMIN")
 
-            .logout(logout -> logout
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/Index")
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .deleteCookies("JSESSIONID")
-                .permitAll()
-            );
+                        .requestMatchers("/reviews/add").hasAnyRole("USER", "ADMIN")
+
+                        .requestMatchers("/Admin/**").hasRole("ADMIN")
+                        .requestMatchers("/AdminProduct/**").hasRole("ADMIN")
+                        .requestMatchers("/AdminUser/**").hasRole("ADMIN")
+                        .requestMatchers("/AdminUser/promote/**").hasRole("ADMIN")
+                        .requestMatchers("/AdminCategories/**").hasRole("ADMIN")
+                        .requestMatchers("/ChangePassword").authenticated()
+
+                        .requestMatchers("/user/profile/upload").authenticated()
+
+                        .anyRequest().authenticated()
+
+                )
+
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/Login")
+                        .failureUrl("/Login?error")
+                        .defaultSuccessUrl("/Index", true)
+                        .permitAll())
+
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/Index")
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .deleteCookies("JSESSIONID")
+                        .permitAll());
 
         return http.build();
     }

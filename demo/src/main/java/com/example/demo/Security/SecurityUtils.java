16292dc212e0303dd81e9b5f4336142d08ec8fb5
hugogarciatapia <h.garciat.2024@alumnos.urjc.es>
2026-05-14 18:38:20 +0200

    comentarios

--- demo/src/main/java/com/example/demo/Security/SecurityUtils.java
+++ demo/src/main/java/com/example/demo/Security/SecurityUtils.java
@@ -5,7 +5,7 @@ import org.owasp.html.Sanitizers;
 
 public class SecurityUtils {
 
-    // Política de sanitización permitiendo formato básico, listas, enlaces y bloques
+    
     public static final PolicyFactory POLICY = Sanitizers.FORMATTING
             .and(Sanitizers.LINKS)
             .and(Sanitizers.BLOCKS);
